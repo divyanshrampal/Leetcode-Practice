128e5bebb296657c53cb2104185bc536d37889e8
divyanshrampal <93667147+divyanshrampal@users.noreply.github.com>
2023-03-16 02:32:46 +0530

    Time: 1 ms (83.14%), Space: 42.3 MB (20.79%) - LeetHub

---
+++ 0958-check-completeness-of-a-binary-tree/0958-check-completeness-of-a-binary-tree.java
@@ -0,0 +1,29 @@
+class Solution {
+  
+  // Define the isCompleteTree function that takes a TreeNode as input and returns a boolean
+  public boolean isCompleteTree(TreeNode root) {
+    // Check if the root node is null, if so, return true (an empty tree is complete)
+    if (root == null)
+      return true;
+
+    // Create a queue to store the nodes of the tree in level order
+    Queue<TreeNode> q = new LinkedList<>(Arrays.asList(root));
+
+    // Traverse the tree in level order
+    while (q.peek() != null) {
+      // Remove the first node from the queue
+      TreeNode node = q.poll();
+      // Add the left and right child nodes of the current node to the queue
+      q.offer(node.left);
+      q.offer(node.right);
+    }
+
+    // Remove any remaining null nodes from the end of the queue
+    while (!q.isEmpty() && q.peek() == null)
+      q.poll();
+
+
+    return q.isEmpty();
+  }
+
+}
