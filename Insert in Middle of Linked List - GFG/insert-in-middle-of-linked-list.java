b8e1eb984b1e64e9187386cd99d1291984c08d4e
divyanshrampal <93667147+divyanshrampal@users.noreply.github.com>
2023-04-03 16:04:05 +0530

    Added solution - LeetHub

---
+++ Insert in Middle of Linked List - GFG/insert-in-middle-of-linked-list.java
@@ -0,0 +1,89 @@
+//{ Driver Code Starts
+import java.util.*;
+import java.io.*;
+
+class Node {
+    int data;
+    Node next;
+    
+    public Node(int data){
+        this.data = data;
+        this.next = null;
+    }
+}
+
+public class Main {
+    public static void main(String[] args){
+        Scanner sc = new Scanner (System.in);
+        int t = sc.nextInt();
+        
+        while(t-->0){
+            int n = sc.nextInt();   
+            Node head = new Node(sc.nextInt());
+            Node tail = head;
+            
+            for(int i=0; i<n-1; i++){
+                tail.next = new Node(sc.nextInt());
+                tail = tail.next;
+            }
+            int key = sc.nextInt();
+            
+            //display(head);
+            Solution obj = new Solution();
+            obj.insertInMid(head, key);
+            display(head);
+            System.out.println();
+        }
+    }
+    
+    public static void display(Node head){
+        Node curr = head;
+        while(curr != null){
+            System.out.print(curr.data+" ");
+            curr = curr.next;
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+/*
+Structure of node class is:
+class Node {
+    int data;
+    Node next;
+    
+    public Node(int data){
+        this.data = data;
+        this.next = null;
+    }
+}
+*/
+class Solution {
+    
+    public Node findMid(Node head){
+        Node slow = head;
+        Node fast = head.next;
+        while(fast != null && fast.next != null){
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        
+        return slow;
+    }
+    
+    public Node insertInMid(Node head, int data){
+        //Insert code here, return the head of modified linked list\
+        
+        Node mid = (head.next != null) ? findMid(head) : head;
+        
+        Node newNode = new Node(data);
+        
+        Node temp = mid.next;
+        mid.next = newNode;
+        newNode.next = temp;
+        
+        return head;
+    }
+}
