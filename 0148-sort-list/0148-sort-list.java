73e13ac9ff496ce3217b50fa7f72fec8ac55bd22
divyanshrampal <93667147+divyanshrampal@users.noreply.github.com>
2023-04-03 14:57:59 +0530

    Time: 9 ms (91.49%), Space: 52.2 MB (22.48%) - LeetHub

---
+++ 0148-sort-list/0148-sort-list.java
@@ -0,0 +1,99 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    
+    public ListNode merge(ListNode first, ListNode second){
+        // if only one element is present in 1st list
+        if(first.next == null){
+            first.next = second;
+            return first;
+        }
+        
+        ListNode curr1 = first;
+        ListNode next1 = first.next;
+        ListNode curr2 = second;
+        
+        while(next1 != null && curr2 != null){
+             if(curr2.val >= curr1.val && curr2.val <= next1.val){
+                 
+                 curr1.next = curr2;
+                 ListNode next2 = curr2.next;
+                 curr2.next = next1;
+                 curr1 = curr2 ;
+                 curr2 = next2;
+                  
+             }
+            else{
+                curr1 = next1;
+                next1 = next1.next;
+                
+                if(next1 == null){
+                    curr1.next = curr2;
+                    return first;
+                }
+            }
+        }
+        
+        return first;
+        
+    }
+    
+    public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+        if(list1 == null){
+            return list2;
+        }
+        
+        if(list2 == null){
+            return list1;
+        }
+        
+        if(list1.val <= list2.val){
+            return merge(list1, list2);
+        }
+        
+        else{
+            return merge(list2, list1);
+        }
+        
+    }
+    
+    public ListNode findMid(ListNode head){
+        ListNode slow = head;
+        ListNode fast = head.next;
+        
+        while(fast != null && fast.next != null){
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        
+        return slow;
+    }
+    
+    public ListNode sortList(ListNode head) {
+        // base case 
+        if(head == null || head.next == null){
+            return head;
+        }
+        
+        ListNode mid = findMid(head);
+        ListNode left = head;
+        ListNode right = mid.next;
+        mid.next = null;
+        
+        left = sortList(left);
+        right = sortList(right);
+        
+        // merger both left and right halves and return
+        
+        return mergeTwoLists(left, right);
+        
+    }
+}
