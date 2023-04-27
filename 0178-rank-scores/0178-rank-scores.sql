d9b32187456473f218db0b540b5a302e3b2b918b
divyanshrampal <93667147+divyanshrampal@users.noreply.github.com>
2023-04-27 09:26:38 +0530

    Time: 1693 ms (5.01%), Space: 0B (100.00%) - LeetHub

---
+++ 0178-rank-scores/0178-rank-scores.sql
@@ -0,0 +1,5 @@
+# Write your MySQL query statement below
+select s1.score, 
+(select count(distinct s2.score) from scores s2 where s1.score <= s2.score) as `Rank`
+from scores s1
+order by s1.score desc;
