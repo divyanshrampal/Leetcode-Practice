a9a6e4d64cc6b3cd6ad9b18bfad8b7fa7ad80e07
divyanshrampal <93667147+divyanshrampal@users.noreply.github.com>
2023-07-30 19:41:29 +0530

    Added solution - LeetHub

---
+++ Search in a Rotated Array - GFG/search-in-a-rotated-array.java
@@ -0,0 +1,71 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+import java.util.*;
+import java.io.*;
+
+public class GFG
+{
+    public static void main(String args[])throws IOException
+    {
+        BufferedReader in=new BufferedReader(new InputStreamReader(System.in));
+        PrintWriter out=new PrintWriter(System.out);
+        
+        int t = Integer.parseInt(in.readLine().trim());
+        while (t-- > 0)
+        {
+            int n = Integer.parseInt(in.readLine().trim());
+            int A[] = new int[n];
+            String s[]=in.readLine().trim().split(" ");
+            for (int i = 0;i < n;i++)
+            {
+                A[i] = Integer.parseInt(s[i]);
+            }
+            int key = Integer.parseInt(in.readLine().trim());
+            
+            out.println(new Solution().search(A, 0, n - 1, key));
+        }
+        out.close();
+    }
+}
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution
+{
+    int search(int nums[], int l, int h, int target)
+    {
+        int left = l;
+        int right = h;
+
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+
+            if (nums[mid] == target) {
+                return mid;
+            }
+
+            // If the left half is sorted
+            if (nums[left] <= nums[mid]) {
+                if (nums[left] <= target && target < nums[mid]) {
+                    right = mid - 1;
+                } else {
+                    left = mid + 1;
+                }
+            }
+            // If the right half is sorted
+            else {
+                if (nums[mid] < target && target <= nums[right]) {
+                    left = mid + 1;
+                } else {
+                    right = mid - 1;
+                }
+            }
+        }
+
+        return -1;
+        
+    }
+}
